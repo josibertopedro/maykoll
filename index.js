8848362f7a42291e5d73f4a6e1760475f36e8526
josibertopedro <josibertopedro@edu.unifor.br>
2023-09-22 20:29:26 -0300

    modificar

--- index.js
+++
@@ -1,8 +0,0 @@
-const express =require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-
-const app = express()
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extend: false}))
